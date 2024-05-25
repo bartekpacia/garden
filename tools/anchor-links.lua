efa12af879a491c901cb84a6a6180c7caa255fe8
Bartek Pacia <barpac02@gmail.com>
2024-05-25 16:36:38 +0100

    skip generating link for the first heading

--- tools/anchor-links.lua
+++ tools/anchor-links.lua
@@ -1,7 +1,15 @@
 -- Inspired by https://github.com/jgm/pandoc-website/pull/50
 
 -- Adds anchor links to headings with IDs.
+
+local i = 0
+
 function Header (h)
+    i = i + 1
+    if i == 1 then
+      return h
+    end
+
     if h.identifier ~= '' then
       -- an empty link to this header
       local anchor_link = pandoc.Link(
