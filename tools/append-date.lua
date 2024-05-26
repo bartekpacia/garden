3944af48e6b38c8c56c9faf986821933b9dc2755
Bartek Pacia <barpac02@gmail.com>
2024-05-27 00:15:03 +0100

    cleanup start

--- tools/append-date.lua
+++ tools/append-date.lua
@@ -3,7 +3,7 @@
 local i = 0
 local envvars = pandoc.system.environment()
 
-pandoc.log.info("Append date called, with DATE = " .. tostring(envvars['DATE']))
+-- pandoc.log.info("Append date called, with DATE = " .. tostring(envvars['DATE']))
 
 -- Adapted from https://stackoverflow.com/a/72762025/7009800
 local function custompara (para)
@@ -15,7 +15,7 @@ local function custompara (para)
   end
 
 function Header (h)
-    pandoc.log.info("Header called, with date " .. tostring(envvars["date"]) .. " and i = " .. i)
+    -- pandoc.log.info("Header called, with date " .. tostring(envvars["date"]) .. " and i = " .. i)
     i = i + 1
     if i == 1 then
         local para = pandoc.Para(pandoc.Str("Published on " .. envvars['DATE']))
